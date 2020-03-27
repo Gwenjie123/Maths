efe8ff4d12156ef416ddf6035d103d275fc3e1ef
Gwenjie123 <62766729+Gwenjie123@users.noreply.github.com>
2020-03-27 21:45:14 +0100

    First Upload

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1585325673,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bgtableau-sheet0.png",
+		"images/spronoff-sheet0.png",
+		"images/spronoff-sheet1.png",
+		"media/school of quirks.m4a",
+		"media/school of quirks.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
